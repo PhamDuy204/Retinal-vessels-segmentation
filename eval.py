7e497aa2f70969169d2ff7832b37867fbe55bf61
Ubuntu <ubuntu@ip-172-31-55-119.us-west-2.compute.internal>
2025-06-03 10:04:15 +0000

    ....

--- eval.py
+++ eval.py
@@ -9,6 +9,7 @@ def eval_for_seg(model,val_loader,gpu_id):
     torch.cuda.set_device(gpu_id)
     with torch.no_grad():
         model.eval()
+        probs= []
         truth_label=[]
         pred_label=[]
         for sample in tqdm(val_loader):
@@ -18,17 +19,19 @@ def eval_for_seg(model,val_loader,gpu_id):
             mask=mask.cuda()
             edge=edge.cuda()
             if check_model_forward_args(model)==2:
-
-                pred_mask = torch.where(model(image,edge)>0.5,1,0).cpu().flatten()
+                prob = model(image,edge)
             else:
-                pred_mask = torch.where(model(image)>0.5,1,0).cpu().flatten()
+                prob = model(image)
+            probs.extend(prob.detach().cpu().flatten().numpy().tolist())
+            pred_mask = torch.where(prob>0.5,1,0).cpu().flatten()
             truth_label.extend(mask.flatten().tolist())
             pred_label.extend(pred_mask.detach().numpy().tolist())
-        truth_label=torch.tensor(truth_label)
-        pred_label= torch.tensor(pred_label)
-        return Accuracy(task='binary')(pred_label,truth_label).item(),BinaryF1Score()(pred_label,truth_label).item(),\
-            JaccardIndex(task='binary')(pred_label,truth_label).item(),Recall(task='binary')(pred_label,truth_label).item(),\
-            Specificity(task='binary')(pred_label,truth_label).item(),\
-            AUROC(task='binary')(pred_label,truth_label).item(),\
-            DiceScore(num_classes=2)(pred_label,truth_label).item()
+        truth_label=torch.tensor(truth_label).cuda()
+        pred_label= torch.tensor(pred_label).cuda()
+        probs = torch.tensor(probs).cuda()
+        return Accuracy(task='binary').cuda()(pred_label,truth_label).item(),BinaryF1Score().cuda()(pred_label,truth_label).item(),\
+            JaccardIndex(task='binary').cuda()(pred_label,truth_label).item(),Recall(task='binary').cuda()(pred_label,truth_label).item(),\
+            Specificity(task='binary').cuda()(pred_label,truth_label).item(),\
+            AUROC(task='binary').cuda()(probs,truth_label).item(),\
+            DiceScore(num_classes=2).cuda()(pred_label,truth_label).item()
             
