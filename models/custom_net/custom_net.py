8888fdc2c6c6b4a608d473cdacf38712a51bdf04
PhamDuy204 <22520337@gm.uit.edu.vn>
2025-08-02 14:22:52 +0700

    updated workspace

--- models/custom_net/custom_net.py
+++ models/custom_net/custom_net.py
@@ -1,32 +1,70 @@
 import os
 import sys
-import torch.nn.functional as F
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from modules import *
+
+import sys
+import torch.nn.functional as F
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# from modules import *
+
 class SegModel(nn.Module):
     def __init__(self, n_channel, n_class):
         super().__init__()
+
         self.n_channel = n_channel
         self.n_class = n_class
 
-        self.first_conv = DoubleConv(n_channel, 64)
-        
-        self.down1 = DownScaling(64, 128)
-        self.down2 = DownScaling(128, 256)
-        self.down3 = DownScaling(256, 512)
-        self.up2 = UpScaling(512, 256)
-        self.up3 = UpScaling(256, 128)
-        self.up4 = UpScaling(128, 64)
+        self.conv1 = convolution(n_channel,64) #b,64,64,64
 
-        self.final_conv = OutConv(64, n_class)
+        self.down1 = nn.Conv2d(64,64,kernel_size=4,stride=2,padding=1,bias=True) #b,64,32,32
 
-    def forward(self, x):
-        x1 = self.first_conv(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x = self.up2(x4, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
+        self.conv2 = convolution(64,128)    #b,128,32,32
+        self.down2 = nn.Conv2d(128,128,kernel_size=4,stride=2,padding=1,bias=True) #b,128,16,16
 
-        return F.sigmoid(self.final_conv(x))
+        self.conv3 = convolution(128,256)   #b,256,16,16
+        self.down3 = nn.Conv2d(256,256,kernel_size=4,stride=2,padding=1,bias=True) #b,256,8,8
+
+        self.bottle_neck = CPSE(256) #b,256,8,8
+
+        self.DGF_3 = DGF(256,256) # b,128,16,16
+        self.MDAE_3 = MDAE()
+
+        self.DGF_2 = DGF(128,128) #b,128,128
+        self.MDAE_2 = MDAE()
+
+        self.DGF_1 = DGF(128,64)
+        self.MDAE_1 = MDAE()
+
+        # self.DGF_2 = DGF(,128)
+        self.b3 = change_feature_size_x4(128,1)
+        self.b2 = change_feature_size_x2(128,1)
+        self.b1 = change_feature_size_x1(128,1)
+
+        self.AWL_func = AWL(3)
+
+    def forward(self,x):
+
+        x_conv1 = self.conv1(x)  #b,64,606,700
+        x_down1 = self.down1(x_conv1) #b,64,304,350
+
+        x_conv2 = self.conv2(x_down1)  #b,128,304,350
+        x_down2 = self.down2(x_conv2)    #b,128,158,176
+
+        x_conv3 = self.conv3(x_down2) #b,256,158,176
+        x_down3 = self.down3(x_conv3) #b,256,80,88
+
+        bottle_neck = self.bottle_neck(x_down3) #b,256,88,88
+
+        x_DGF_3 = self.DGF_3(bottle_neck,x_conv3)  #b,256,158,176
+        # print(x_DGF_3.shape)
+        x_MDAE_3 =  self.MDAE_3(x_DGF_3)  #b,256,158,176
+
+        x_DGF_2 = self.DGF_2(x_DGF_3,x_conv2)
+        x_MDAE_2 = self.MDAE_2(x_DGF_2)
+
+        x_DGF_1 = self.DGF_1(x_DGF_2,x_conv1)
+        x_MDAE_1 = self.MDAE_1(x_DGF_1)
+
+        out = torch.sigmoid(self.AWL_func(self.b3(x_MDAE_3),self.b2(x_MDAE_2),self.b1(x_MDAE_1)))
+        return out
