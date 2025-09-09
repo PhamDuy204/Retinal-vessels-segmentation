c11a3432f2f5c109700dec8f3b760b1c38d734a1
HuaTanSang <huatansang2004@gmail.com>
2025-09-09 22:53:17 +0700

    add super

--- models/custom_net/bottle_neck.py
+++ models/custom_net/bottle_neck.py
@@ -309,6 +309,7 @@ class CustomBottleNeck(nn.Module):
 
 class Depthwise(nn.Module): 
     def __init__(self, in_channels, out_channels, kernel_size): 
+        super(Depthwise, self).__init__() 
         self.depthwise = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
                                    groups=in_channels, kernel_size=kernel_size, padding='same') 
         self.group_norm = nn.GroupNorm(num_groups=in_channels, num_channels=in_channels, affine=False)
