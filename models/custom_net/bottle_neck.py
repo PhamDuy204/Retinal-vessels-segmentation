184c0aa4ebe9c08c52656de4a4c8376a14d4bff7
HuaTanSang <huatansang2004@gmail.com>
2025-09-09 21:44:37 +0700

    add bottle neck

--- models/custom_net/bottle_neck.py
+++ models/custom_net/bottle_neck.py
@@ -6,38 +6,39 @@ import math
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange, repeat
-from mamba_ssm.ops.selective_scan_interface import selective_scan_fn
-from mamba_ssm import Mamba2
-class FeedForward(nn.Module):
-    def __init__(self, dimension,dropout=0.0):
-        super().__init__()
-        self.network = nn.Sequential(
-            nn.Linear(dimension, dimension * 4),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(dimension * 4,dimension),
-        )
+# from mamba_ssm.ops.selective_scan_interface import selective_scan_fn
+# from mamba_ssm import Mamba2
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.network(x)
-class SoftMoe(nn.Module):
-    def __init__(self, dimension,n_experts=8,slots_per_expert=2,dropout=0.0):
-        super().__init__()
-        self.experts = nn.ModuleList([FeedForward(dimension,dropout) for _ in range(n_experts)])
-        self.phi = nn.Parameter(torch.randn(dimension, n_experts * slots_per_expert))
-        self.slots_per_expert=slots_per_expert
-    def forward(self, x: torch.Tensor):
-        logits = torch.matmul(x, self.phi) # (batch_size, seq_len, slots)
-        dispatch_weights = F.softmax(logits, dim=-1)
-        combine_weights = F.softmax(logits, dim=1)
-        xs = torch.bmm(dispatch_weights.transpose(1, 2), x)
-        ys = torch.cat(
-            [expert(xs[:, i * self.slots_per_expert : (i + 1) * self.slots_per_expert, :]) 
-                          for i, expert in enumerate(self.experts)],
-            dim=1
-            )
-        y = torch.bmm(combine_weights, ys)
-        return y
+# class FeedForward(nn.Module):
+#     def __init__(self, dimension,dropout=0.0):
+#         super().__init__()
+#         self.network = nn.Sequential(
+#             nn.Linear(dimension, dimension * 4),
+#             nn.GELU(),
+#             nn.Dropout(dropout),
+#             nn.Linear(dimension * 4,dimension),
+#         )
+
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         return self.network(x)
+# class SoftMoe(nn.Module):
+#     def __init__(self, dimension,n_experts=8,slots_per_expert=2,dropout=0.0):
+#         super().__init__()
+#         self.experts = nn.ModuleList([FeedForward(dimension,dropout) for _ in range(n_experts)])
+#         self.phi = nn.Parameter(torch.randn(dimension, n_experts * slots_per_expert))
+#         self.slots_per_expert=slots_per_expert
+#     def forward(self, x: torch.Tensor):
+#         logits = torch.matmul(x, self.phi) # (batch_size, seq_len, slots)
+#         dispatch_weights = F.softmax(logits, dim=-1)
+#         combine_weights = F.softmax(logits, dim=1)
+#         xs = torch.bmm(dispatch_weights.transpose(1, 2), x)
+#         ys = torch.cat(
+#             [expert(xs[:, i * self.slots_per_expert : (i + 1) * self.slots_per_expert, :]) 
+#                           for i, expert in enumerate(self.experts)],
+#             dim=1
+#             )
+#         y = torch.bmm(combine_weights, ys)
+#         return y
 
 # class MambaVisionMixer(nn.Module):
 #     def __init__(self, dim, d_state=16, kernel_size=3):
@@ -282,23 +283,104 @@ class CustomBottleNeck(nn.Module):
 #         second_stage=self.mha(first_stage)
 #         second_stage=second_stage.permute(0,2,3,1).contiguous().flatten(1,2)
 #         return (second_stage+self.ff_1(second_stage)).view(b,h,w,c).permute(0,3,1,2).contiguous()
-class BottleNeck(nn.Module):
-    def __init__(self, dimension,n_heads=1,dropout=0.0,n_experts=8,slots_per_expert=2,d_state=16):
-        super().__init__()
-        self.pre_norm=nn.LayerNorm(dimension,bias=False)
-        self.post_norm=nn.LayerNorm(dimension,bias=False)
-        self.mamba=Mamba2(dimension,d_state,conv_bias=False)
-        self.ff_0=SoftMoe(dimension,n_experts,slots_per_expert,dropout)
-        self.mha=MultiHeadAttention(dimension,n_heads,dropout)
-        self.ff_1=SoftMoe(dimension,n_experts,slots_per_expert,dropout)
+# class BottleNeck(nn.Module):
+#     def __init__(self, dimension,n_heads=1,dropout=0.0,n_experts=8,slots_per_expert=2,d_state=16):
+#         super().__init__()
+#         self.pre_norm=nn.LayerNorm(dimension,bias=False)
+#         self.post_norm=nn.LayerNorm(dimension,bias=False)
+#         self.mamba=Mamba2(dimension,d_state,conv_bias=False)
+#         self.ff_0=SoftMoe(dimension,n_experts,slots_per_expert,dropout)
+#         self.mha=MultiHeadAttention(dimension,n_heads,dropout)
+#         self.ff_1=SoftMoe(dimension,n_experts,slots_per_expert,dropout)
 
-    def forward(self, x: torch.Tensor):
-        b,c,h, w = x.shape
-        x=x.permute(0,2,3,1).contiguous().flatten(1,2)
-        mamba=self.mamba(self.pre_norm(x))+x
-        # print(mamba)
-        first_stage=(self.ff_0(self.post_norm(mamba))+mamba).view(b,h,w,c).permute(0,3,1,2).contiguous()
-        second_stage=self.mha(first_stage)
-        second_stage=second_stage.permute(0,2,3,1).contiguous().flatten(1,2)
-        return (second_stage+self.ff_1(second_stage)).view(b,h,w,c).permute(0,3,1,2).contiguous()
+#     def forward(self, x: torch.Tensor):
+#         b,c,h, w = x.shape
+#         x=x.permute(0,2,3,1).contiguous().flatten(1,2)
+#         mamba=self.mamba(self.pre_norm(x))+x
+#         # print(mamba)
+#         first_stage=(self.ff_0(self.post_norm(mamba))+mamba).view(b,h,w,c).permute(0,3,1,2).contiguous()
+#         second_stage=self.mha(first_stage)
+#         second_stage=second_stage.permute(0,2,3,1).contiguous().flatten(1,2)
+#         return (second_stage+self.ff_1(second_stage)).view(b,h,w,c).permute(0,3,1,2).contiguous()
 
+
+################################################ BOTTLE NECK II#############################################
+
+
+class Depthwise(nn.Module): 
+    def __init__(self, in_channels, out_channels, kernel_size): 
+        self.depthwise = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
+                                   groups=in_channels, kernel_size=kernel_size, padding='same') 
+        self.group_norm = nn.GroupNorm(num_groups=in_channels, num_channels=in_channels, affine=False)
+        self.pointwise = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
+                                   kernel_size=1, padding='same')
+        
+    def forward(self, X): 
+        x = self.depthwise(X) 
+        x = self.group_norm(x) 
+        x = self.pointwise(x) 
+
+        return x 
+
+
+
+
+class VGG(nn.Module): 
+    def __init__(self, in_channels, out_channels): 
+        super(VGG, self).__init__() 
+
+        self.conv1 = Depthwise(in_channels=in_channels, out_channels=in_channels, kernel_size=3)
+        self.gelu1 = nn.GELU()
+        self.conv2 = Depthwise(in_channels=in_channels, out_channels=out_channels, kernel_size=5)
+        self.gelu2 = nn.GELU() 
+
+
+    def forward(self, X): 
+        x = self.conv1(X) 
+        x = self.gelu1(x) 
+        x = self.conv2(x) 
+        x = self.gelu2(x) 
+
+        return x 
+
+
+class ResNet(nn.Module): 
+    def __init__(self, in_channels, out_channels): 
+        super(ResNet, self).__init__() 
+        
+        self.conv1 = Depthwise(in_channels=in_channels, out_channels=in_channels, kernel_size=3) 
+        self.gelu1 = nn.GELU()
+        self.conv2 = Depthwise(in_channels=in_channels, out_channels=out_channels, kernel_size=5)
+        self.gelu2 = nn.GELU() 
+
+    def forward(self, X): 
+        x = self.conv1(X) 
+        x = self.gelu1(x) 
+        x = self.conv2(x) 
+        x = x + X 
+        x = self.gelu2(x) 
+        
+        return x 
+
+
+
+class CustomBottleNeck1(nn.Module): 
+    def __init__(self, in_channels, out_channels): 
+        super(CustomBottleNeck1, self).__init__() 
+
+        self.vgg = VGG(in_channels=in_channels, out_channels=in_channels*2)
+        self.resnet = ResNet(in_channels=in_channels, out_channels=in_channels*2)
+        self.gelu = nn.GELU() 
+        self.conv1 = nn.Conv2d(in_channels=in_channels*2, out_channels=out_channels, kernel_size=1, padding='same')
+
+    def forward(self, X): 
+
+        x1 = self.vgg(X) 
+        x2 = self.resnet(X) 
+
+        x = self.gelu(x1 + x2 - x1 * x2)
+        out = self.conv1(x) 
+
+        return out 
+
+        
