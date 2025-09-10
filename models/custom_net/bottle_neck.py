860a35a581b875c61406234c38ca5985b3cec29a
HuaTanSang <huatansang2004@gmail.com>
2025-09-10 15:39:31 +0700

    adjust depthwise

--- models/custom_net/bottle_neck.py
+++ models/custom_net/bottle_neck.py
@@ -310,7 +310,7 @@ class CustomBottleNeck(nn.Module):
 class Depthwise(nn.Module): 
     def __init__(self, in_channels, out_channels, kernel_size): 
         super(Depthwise, self).__init__() 
-        self.depthwise = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
+        self.depthwise = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, 
                                    groups=in_channels, kernel_size=kernel_size, padding='same') 
         self.group_norm = nn.GroupNorm(num_groups=in_channels, num_channels=in_channels, affine=False)
         self.pointwise = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
