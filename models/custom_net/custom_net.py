242781ce0d5920340c15504e9968ea00ce0e914f
HuaTanSang <huatansang2004@gmail.com>
2025-10-17 14:16:33 +0700

    fix

--- models/custom_net/custom_net.py
+++ models/custom_net/custom_net.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from modules import *
-from bottle_neck import BottleNeck,CAB
+# from bottle_neck import BottleNeck,CAB
 from bottle_neck_1 import CustomBottleNeck1
 # import sys
 # import torch.nn.functional as F
