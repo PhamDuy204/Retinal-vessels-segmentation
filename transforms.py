e5812aeb9d59959f53b7ef371773ac79426c52c2
PhamDuy204 <139705394+PhamDuy204@users.noreply.github.com>
2026-02-01 17:49:32 +0700

    update vs

--- transforms.py
+++ transforms.py
@@ -34,14 +34,19 @@ def get_train_patch_transforms():
             A.HorizontalFlip(p=1.0),
             A.VerticalFlip(p=1.0)
         ], p=0.5),
-        A.Normalize(mean=(0,),std=(1,)),
+        A.Normalize(mean=(0.,),std=(1.,)),
         ToTensorV2()
     ])
 
 def get_test_patch_transforms():
     return A.Compose([
-        A.Normalize(mean=(0,),std=(1,)),
+        A.Normalize(mean=(0.,),std=(1.,)),
         ToTensorV2()
     ])
 
+def get_test_patch_transforms_v1():
+    return A.Compose([
+        A.Normalize(mean=(0.5,),std=(0.5,)),
+        ToTensorV2()
+    ])
  
